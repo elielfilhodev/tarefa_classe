2690585280c3f3d65a44855733ea184a17299e69
Eliel Filho <elielfilho9@outlook.com>
2026-08-18 20:10:33 -0300

    criação da classe

---
+++ src/Notebook.java
@@ -0,0 +1,9 @@
+public class Notebook {
+    public static void main(String[] args) {
+        Objeto meuNotebook = new Objeto();
+        meuNotebook.modelo = "Lenovo";
+        meuNotebook.gigas = 32;
+
+        System.out.println(meuNotebook.modelo + " - " + meuNotebook.gigas);
+    }
+}
---
+++ src/Objeto.java
@@ -0,0 +1,5 @@
+// 1. O molde (Classe)
+public class Objeto {
+    String modelo;
+    int gigas;
+}
